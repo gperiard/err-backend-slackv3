26264d78306f48903c27ad600ec9bc0fa10003e0
Gabriel Périard-Tremblay <gabriel@periard.ca>
2021-11-09 23:45:55 -0500

    Fix webapi api_call function

--- slackv3.py
+++ slackv3.py
@@ -117,7 +117,7 @@ class SlackBackend(ErrBot):
         if data is None:
             data = {}
 
-        response = self.slack_web.api_call(method, **data)
+        response = self.slack_web.api_call(method, params=data)
 
         if raise_errors and not response["ok"]:
             raise SlackAPIResponseError(
